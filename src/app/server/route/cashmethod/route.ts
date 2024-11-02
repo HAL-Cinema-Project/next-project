46ed5b146f548724d5a91d16c38b195715f3376f
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-11-02 23:14:27 +0900

    fix cashmethod crudapi

--- src/app/server/route/cashmethod/route.ts
+++ src/app/server/route/cashmethod/route.ts
@@ -1,16 +1,11 @@
 // cashmethodモデルのAPIを定義
+import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse, NextRequest } from 'next/server';
 import { Pool } from 'pg';
 
 // PSQL接続情報
-const pool = new Pool({
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
-	database: process.env.DB_NAME,
-});
+const prisma = new PrismaClient();
 
 interface CashMethod {
 	method_id: number;
@@ -19,18 +14,15 @@ interface CashMethod {
 
 // GETメソッドの処理
 export async function GET() {
-	const client = await pool.connect();
 	try {
-		const ret = await client.query('SELECT * FROM "CashMethod"', []);
-		return NextResponse.json(ret.rows);
+		const categories = await prisma.cashMethod.findMany();
+		return NextResponse.json(categories);
 	} catch (error) {
 		console.error('Error executing query', error);
 		return NextResponse.json(
 			{ error: 'Error executing query' },
 			{ status: 500 }
 		);
-	} finally {
-		client.release();
 	}
 }
 
@@ -39,23 +31,19 @@ export async function POST(req: NextRequest) {
 	try {
 		const { method }: CashMethod = await req.json();
 
-		const client = await pool.connect();
 		try {
-			const query = `
-        INSERT INTO "CashMethod" (method) 
-        VALUES ($1) 
-        RETURNING *`;
-			const values = [method];
-			const result = await client.query(query, values);
-			return NextResponse.json(result.rows[0], { status: 201 });
+			const newInquiry = await prisma.cashMethod.create({
+				data: {
+					method,
+				},
+			});
+			return NextResponse.json(newInquiry, { status: 201 });
 		} catch (error) {
 			console.error('Error executing query', error);
 			return NextResponse.json(
 				{ error: 'Error executing query' },
 				{ status: 500 }
 			);
-		} finally {
-			client.release();
 		}
 	} catch (error) {
 		console.error('Invalid request payload', error);
