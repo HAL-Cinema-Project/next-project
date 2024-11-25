4ab74ca47d86755421cd298682f90fd199c0a3c3
Koshi Yasumura <kirara397.kr@gmail.com>
2024-11-25 10:39:27 +0900

    とりあえず投げ

--- src/app/admin/inquiry_admin/page.tsx
+++ src/app/admin/inquiry_admin/page.tsx
@@ -1,7 +1,23 @@
+import { AdminInquirySort } from '@/app/_components/block/AdminInquirySort';
+import { AdminCaption } from '@/app/_layouts/block/AdminCaption';
+import { Box } from '@yamada-ui/react';
 import React from 'react';
+import { AdminInquiryAccordion } from './components/AdminInquiryAccodion';
 
 const page = () => {
-	return <div>page</div>;
+	const title = '問い合わせ管理';
+
+	return (
+		<>
+			<Box w={'1200px'} m={'auto'}>
+				<Box padding={'20px'}>
+					<AdminCaption title={title} />
+					<AdminInquirySort />
+					<AdminInquiryAccordion />
+				</Box>
+			</Box>
+		</>
+	);
 };
 
 export default page;
